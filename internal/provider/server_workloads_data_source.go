1f97472c32b79da64589dbf70ecc77dc85779b9e
Jason Kwon <jkwon@aembit.io>
2024-02-14 20:58:17 -0800

    OPS-683: Add additional schema fields for Server Workloads

--- internal/provider/server_workloads_data_source.go
+++ internal/provider/server_workloads_data_source.go
@@ -69,9 +69,33 @@ func (r *serverWorkloadsDataSource) Schema(_ context.Context, _ datasource.Schem
 							Description: "User-provided name of the server workload.",
 							Computed:    true,
 						},
+						"description": schema.StringAttribute{
+							Description: "User-provided description of the server workload.",
+							Computed:    true,
+						},
+						"is_active": schema.BoolAttribute{
+							Description: "Active/Inactive status of the server workload.",
+							Computed:    true,
+						},
 						"type": schema.StringAttribute{
 							Description: "Type of server workload.",
 							Computed:    true,
+						},
+						"tags": schema.ListNestedAttribute{
+							Description: "List of Tags.",
+							Computed:    true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"key": schema.StringAttribute{
+										Description: "Tag key.",
+										Computed:    true,
+									},
+									"value": schema.StringAttribute{
+										Description: "Tag value.",
+										Computed:    true,
+									},
+								},
+							},
 						},
 						"service_endpoint": schema.SingleNestedAttribute{
 							Description: "Service endpoint details.",
@@ -79,6 +103,10 @@ func (r *serverWorkloadsDataSource) Schema(_ context.Context, _ datasource.Schem
 							Attributes: map[string]schema.Attribute{
 								"external_id": schema.StringAttribute{
 									Description: "Alphanumeric identifier of the service endpoint.",
+									Computed:    true,
+								},
+								"id": schema.Int64Attribute{
+									Description: "Number identifier of the service endpoint.",
 									Computed:    true,
 								},
 								"host": schema.StringAttribute{
@@ -97,6 +125,10 @@ func (r *serverWorkloadsDataSource) Schema(_ context.Context, _ datasource.Schem
 									Description: "hostname of the service endpoint.",
 									Computed:    true,
 								},
+								"requested_tls": schema.BoolAttribute{
+									Description: "hostname of the service endpoint.",
+									Computed:    true,
+								},
 								"tls_verification": schema.StringAttribute{
 									Description: "hostname of the service endpoint.",
 									Computed:    true,
@@ -104,6 +136,44 @@ func (r *serverWorkloadsDataSource) Schema(_ context.Context, _ datasource.Schem
 								"transport_protocol": schema.StringAttribute{
 									Description: "hostname of the service endpoint.",
 									Computed:    true,
+								},
+								"tls": schema.BoolAttribute{
+									Description: "hostname of the service endpoint.",
+									Computed:    true,
+								},
+								"http_headers": schema.ListNestedAttribute{
+									Description: "List of HTTP headers.",
+									Computed:    true,
+									NestedObject: schema.NestedAttributeObject{
+										Attributes: map[string]schema.Attribute{
+											"key": schema.StringAttribute{
+												Description: "HTTP Header Key.",
+												Computed:    true,
+											},
+											"value": schema.StringAttribute{
+												Description: "HTTP Header value.",
+												Computed:    true,
+											},
+										},
+									},
+								},
+								"workload_service_authentication": schema.SingleNestedAttribute{
+									Description: "Service authentication details.",
+									Computed:    true,
+									Attributes: map[string]schema.Attribute{
+										"method": schema.StringAttribute{
+											Description: "Service authentication method.",
+											Computed:    true,
+										},
+										"scheme": schema.StringAttribute{
+											Description: "Service authentication method.",
+											Computed:    true,
+										},
+										"config": schema.StringAttribute{
+											Description: "Service authentication method.",
+											Computed:    true,
+										},
+									},
 								},
 							},
 						},
@@ -130,19 +200,50 @@ func (d *serverWorkloadsDataSource) Read(ctx context.Context, req datasource.Rea
 	// Map response body to model
 	for _, server_workload := range server_workloads {
 		serverWorkloadState := serverWorkloadResourceModel{
-			ID:   types.StringValue(server_workload.EntityDTO.ExternalId),
-			Name: types.StringValue(server_workload.EntityDTO.Name),
-			Type: types.StringValue(server_workload.Type),
+			ID:          types.StringValue(server_workload.EntityDTO.ExternalId),
+			Name:        types.StringValue(server_workload.EntityDTO.Name),
+			Description: types.StringValue(server_workload.EntityDTO.Description),
+			IsActive:    types.BoolValue(server_workload.EntityDTO.IsActive),
+			Type:        types.StringValue(server_workload.Type),
+		}
+
+		for _, tag := range server_workload.EntityDTO.Tags {
+			tagState := tagModel{
+				Key:   types.StringValue(tag.Key),
+				Value: types.StringValue(tag.Value),
+			}
+
+			serverWorkloadState.Tags = append(serverWorkloadState.Tags, tagState)
 		}
 
 		serverWorkloadState.ServiceEndpoint = &serviceEndpointModel{
 			ExternalId:        types.StringValue(server_workload.ServiceEndpoint.ExternalId),
+			Id:                types.Int64Value(int64(server_workload.ServiceEndpoint.Id)),
 			Host:              types.StringValue(server_workload.ServiceEndpoint.Host),
-			Port:              types.Int64Value(int64(server_workload.ServiceEndpoint.Port)),
 			AppProtocol:       types.StringValue(server_workload.ServiceEndpoint.AppProtocol),
 			TransportProtocol: types.StringValue(server_workload.ServiceEndpoint.TransportProtocol),
 			RequestedPort:     types.Int64Value(int64(server_workload.ServiceEndpoint.RequestedPort)),
+			RequestedTls:      types.BoolValue(server_workload.ServiceEndpoint.RequestedTls),
+			Port:              types.Int64Value(int64(server_workload.ServiceEndpoint.Port)),
+			Tls:               types.BoolValue(server_workload.ServiceEndpoint.Tls),
 			TlsVerification:   types.StringValue(server_workload.ServiceEndpoint.TlsVerification),
+		}
+
+		for _, header := range server_workload.ServiceEndpoint.HttpHeaders {
+			headerState := keyValueModel{
+				Key:   types.StringValue(header.Key),
+				Value: types.StringValue(header.Value),
+			}
+
+			serverWorkloadState.ServiceEndpoint.HttpHeaders = append(serverWorkloadState.ServiceEndpoint.HttpHeaders, headerState)
+		}
+
+		if server_workload.ServiceEndpoint.WorkloadServiceAuthentication != nil {
+			serverWorkloadState.ServiceEndpoint.WorkloadServiceAuthentication = &workloadServiceAuthenticationModel{
+				Method: types.StringValue(server_workload.ServiceEndpoint.WorkloadServiceAuthentication.Method),
+				Scheme: types.StringValue(server_workload.ServiceEndpoint.WorkloadServiceAuthentication.Scheme),
+				Config: types.StringValue(server_workload.ServiceEndpoint.WorkloadServiceAuthentication.Config),
+			}
 		}
 
 		state.ServerWorkloads = append(state.ServerWorkloads, serverWorkloadState)
