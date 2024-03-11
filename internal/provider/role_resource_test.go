41c03f7e68898e9f49b384e078bb57bd695f707d
David Walker <david.walker@quantifex.com>
2024-03-11 09:50:17 -0400

    BAC-2638: Unique names in Role acceptance tests

--- internal/provider/role_resource_test.go
+++ internal/provider/role_resource_test.go
@@ -1,7 +1,10 @@
 package provider
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -11,12 +14,16 @@ func TestAccRoleResource(t *testing.T) {
 	createFile, _ := os.ReadFile("../../tests/roles/TestAccRoleResource.tf")
 	modifyFile, _ := os.ReadFile("../../tests/roles/TestAccRoleResource.tfmod")
 
+	randID := rand.Intn(10000000)
+	createFileConfig := strings.ReplaceAll(string(createFile), "unittest1namespace", fmt.Sprintf("unittest1namespace%d", randID))
+	modifyFileConfig := strings.ReplaceAll(string(modifyFile), "unittest1namespace", fmt.Sprintf("unittest1namespace%d", randID))
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: string(createFile),
+				Config: string(createFileConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify Trust Provider Name
 					resource.TestCheckResourceAttr("aembit_role.role", "name", "TF Acceptance Role"),
@@ -34,7 +41,7 @@ func TestAccRoleResource(t *testing.T) {
 			},
 			// Update and Read testing
 			{
-				Config: string(modifyFile),
+				Config: string(modifyFileConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify Name updated
 					resource.TestCheckResourceAttr("aembit_role.role", "name", "TF Acceptance Role - Modified"),
